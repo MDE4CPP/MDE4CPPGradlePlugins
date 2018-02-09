abf9f44795e835453cf8c041ab6955612be7bd43
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-02-10 00:58:09 +0100

    exchange content of GradlePropertyAnalyser::isStructureBuildRequested and GradlePropertyAnalyser::isExecutionBuildRequested

--- MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/GradlePropertyAnalyser.java
+++ MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/GradlePropertyAnalyser.java
@@ -27,16 +27,16 @@ class GradlePropertyAnalyser
 		return (project.hasProperty("RELEASE") && project.property("RELEASE") != "0") || (project.hasProperty("R") && project.property("R") != "0")
 				|| (!project.hasProperty("RELEASE") && !project.hasProperty("R") && !project.hasProperty("DEBUG") && !project.hasProperty("D"));
 	}
-	
+
 	static boolean isExecutionBuildRequested(Project project)
 	{
-		return (project.hasProperty("STRUCTURE") && project.property("STRUCTURE") != "0") || (project.hasProperty("S") && project.property("S") != "0")
+		return (project.hasProperty("EXECUTION") && project.property("EXECUTION") != "0") || (project.hasProperty("E") && project.property("E") != "0")
 				|| (!project.hasProperty("STRUCTURE") && !project.hasProperty("S") && !project.hasProperty("EXECUTION") && !project.hasProperty("E"));
 	}
-	
+
 	static boolean isStructureBuildRequested(Project project)
 	{
-		return (project.hasProperty("EXECUTION") && project.property("EXECUTION") != "0") || (project.hasProperty("E") && project.property("E") != "0")
+		return (project.hasProperty("STRUCTURE") && project.property("STRUCTURE") != "0") || (project.hasProperty("S") && project.property("S") != "0")
 				|| (!project.hasProperty("STRUCTURE") && !project.hasProperty("S") && !project.hasProperty("EXECUTION") && !project.hasProperty("E"));
 	}
 }
