97dba570a09a167382b45e594c9c46c18e5ffb66
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-02-09 20:03:18 +0100

    extract ProcessInputStreamThread into own file

--- MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/MDE4CPPCompile.java
+++ MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/MDE4CPPCompile.java
@@ -1,9 +1,6 @@
 package tui.sse.mde4cpp;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -171,52 +168,6 @@ public class MDE4CPPCompile extends DefaultTask
 		if (isReleaseModeActive())
 		{
 			compileBuildMode(BUILD_MODE.RELEASE);
-		}
-	}
-
-	private class ProcessInputStreamThread extends Thread
-	{
-		private InputStream m_stream;
-		private boolean m_isErrorSteam = false;
-		private String m_message;
-
-		private ProcessInputStreamThread(InputStream steam, boolean isErrorStream, String message)
-		{
-			m_stream = steam;
-			m_isErrorSteam = isErrorStream;
-			m_message = message;
-		}
-
-		@Override
-		public void run()
-		{
-			try
-			{
-				BufferedReader reader = new BufferedReader(new InputStreamReader(m_stream));
-				String line;
-				if (m_message != null)
-				{
-					String highlighting = new String(new char[m_message.length()+4]).replace('\0', '#');
-					System.out.println(highlighting);
-					System.out.println("# " + m_message + " #");
-					System.out.println(highlighting);
-				}
-				while ((line = reader.readLine()) != null)
-				{
-					if (m_isErrorSteam)
-					{
-						System.err.println(line);
-					}
-					else
-					{
-						System.out.println(line);
-					}
-				}
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
 		}
 	}
 }
