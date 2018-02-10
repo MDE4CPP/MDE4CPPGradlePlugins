1cb944274e5aae49186d55ae330353f388584765
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-02-10 01:11:03 +0100

    fix property check: replace property != "0" with !property.equals("0")

--- MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/GradlePropertyAnalyser.java
+++ MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/GradlePropertyAnalyser.java
@@ -18,25 +18,25 @@ class GradlePropertyAnalyser
 
 	static boolean isDebugBuildModeRequestet(Project project)
 	{
-		return (project.hasProperty("DEBUG") && project.property("DEBUG") != "0") || (project.hasProperty("D") && project.property("D") != "0")
+		return (project.hasProperty("DEBUG") && (!project.property("DEBUG").equals("0"))) || (project.hasProperty("D") && (!project.property("D").equals("0")))
 				|| (!project.hasProperty("RELEASE") && !project.hasProperty("R") && !project.hasProperty("DEBUG") && !project.hasProperty("D"));
 	}
 
 	static boolean isReleaseBuildModeRequested(Project project)
 	{
-		return (project.hasProperty("RELEASE") && project.property("RELEASE") != "0") || (project.hasProperty("R") && project.property("R") != "0")
+		return (project.hasProperty("RELEASE") && (!project.property("RELEASE").equals("0"))) || (project.hasProperty("R") && (!project.property("R").equals("0")))
 				|| (!project.hasProperty("RELEASE") && !project.hasProperty("R") && !project.hasProperty("DEBUG") && !project.hasProperty("D"));
 	}
 
 	static boolean isExecutionBuildRequested(Project project)
 	{
-		return (project.hasProperty("EXECUTION") && project.property("EXECUTION") != "0") || (project.hasProperty("E") && project.property("E") != "0")
+		return (project.hasProperty("EXECUTION") && (!project.property("EXECUTION").equals("0"))) || (project.hasProperty("E") && (!project.property("E").equals("0")))
 				|| (!project.hasProperty("STRUCTURE") && !project.hasProperty("S") && !project.hasProperty("EXECUTION") && !project.hasProperty("E"));
 	}
 
 	static boolean isStructureBuildRequested(Project project)
 	{
-		return (project.hasProperty("STRUCTURE") && project.property("STRUCTURE") != "0") || (project.hasProperty("S") && project.property("S") != "0")
+		return (project.hasProperty("STRUCTURE") && (!project.property("STRUCTURE").equals("0"))) || (project.hasProperty("S") && (!project.property("S").equals("0")))
 				|| (!project.hasProperty("STRUCTURE") && !project.hasProperty("S") && !project.hasProperty("EXECUTION") && !project.hasProperty("E"));
 	}
 }
