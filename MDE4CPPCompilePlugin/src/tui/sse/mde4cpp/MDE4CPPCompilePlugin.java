e9754f6885daafc93b332ff88996824191b70845
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-02-10 03:08:10 +0100

    add documentation to MDE4CPPCompilePlugin

--- MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/MDE4CPPCompilePlugin.java
+++ MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/MDE4CPPCompilePlugin.java
@@ -5,6 +5,10 @@ import java.io.File;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
+/**
+ * This class represents the MDE4CPPCompile-Plugin extension to Gradle.
+ *
+ */
 public class MDE4CPPCompilePlugin implements Plugin<Project>
 {
 	@Override
