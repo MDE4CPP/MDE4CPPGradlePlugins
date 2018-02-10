4465b6d68bcf1516aefbce2457eacb4b020e07a5
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-02-10 04:11:49 +0100

    add documentation to CommandBuilder

--- MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/CommandBuilder.java
+++ MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/CommandBuilder.java
@@ -6,8 +6,20 @@ import java.util.List;
 
 import org.gradle.api.Project;
 
+/**
+ * Class for the creation of C++ compile command using cmake and gnu.<br>
+ * <br>
+ * This class provides operation to get cmake and make command for a C++ project
+ * depending on the operation system.
+ * 
+ */
 class CommandBuilder
 {
+	/**
+	 * Returns cmake generator name depending on operation system
+	 * 
+	 * @return generator name
+	 */
 	private static String getCMakeGenerator()
 	{
 		if (isWindowsSystem())
@@ -20,6 +32,11 @@ class CommandBuilder
 		}
 	}
 
+	/**
+	 * Creates initial command list depending on operation system.
+	 * 
+	 * @return command list
+	 */
 	static private List<String> initialCommandList()
 	{
 		List<String> commandList = new LinkedList<>();
@@ -31,11 +48,26 @@ class CommandBuilder
 		return commandList;
 	}
 
+	/**
+	 * check, if operation system is a windows system
+	 * 
+	 * @return true, if current system is windows based, otherwise false
+	 */
 	private static boolean isWindowsSystem()
 	{
 		return System.getProperty("os.name").toLowerCase().contains("windows");
 	}
 
+	/**
+	 * Creates specific cmake command including build mode and project folder
+	 * depending in operation system
+	 * 
+	 * @param buildMode
+	 *            build mode, which should be used for compiling
+	 * @param projectFolder
+	 *            the project folder containing CMakeLists.txt
+	 * @return full specified cmake command
+	 */
 	static List<String> getCMakeCommand(BUILD_MODE buildMode, File projectFolder)
 	{
 		List<String> commandList = CommandBuilder.initialCommandList();
@@ -43,6 +75,14 @@ class CommandBuilder
 		return commandList;
 	}
 
+	/**
+	 * Creates specific make command including parallel jobs option depending in
+	 * operation system
+	 * 
+	 * @param project
+	 *            current project instance contains existing properties
+	 * @return full specified make command
+	 */
 	static List<String> getMakeCommand(Project project)
 	{
 		List<String> commandList = CommandBuilder.initialCommandList();
@@ -50,6 +90,11 @@ class CommandBuilder
 		return commandList;
 	}
 
+	/**
+	 * returns the gnu make tool depending on operation system
+	 * 
+	 * @return gnu make tool
+	 */
 	static String getMakeTool()
 	{
 		if (isWindowsSystem())
