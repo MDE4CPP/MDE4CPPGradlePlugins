3b5016ca2bd51ac3d638428b6db0d0abba5a5f09
wichmann <Alexander.Wichmann@tu-ilmenau.de>
2018-09-14 23:21:33 +0200

    [MDE4CPPCompilePlugin] remove general task definition

--- MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/MDE4CPPCompilePlugin.java
+++ MDE4CPPCompilePlugin/src/tui/sse/mde4cpp/MDE4CPPCompilePlugin.java
@@ -1,7 +1,5 @@
 package tui.sse.mde4cpp;
 
-import java.io.File;
-
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -14,9 +12,5 @@ public class MDE4CPPCompilePlugin implements Plugin<Project>
 	@Override
 	public void apply(Project project)
 	{
-		project.getTasks().create("compile", MDE4CPPCompile.class, (task) ->
-		{
-			task.setProjectFolder(new File(".").getAbsolutePath());
-		});
 	}
 }
